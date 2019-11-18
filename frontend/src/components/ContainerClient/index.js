648d517669b0192474ea15a7aea480f1d50a8568
kielkow <matheuskiel@fiorifer.com.br>
2019-11-18 17:41:11 -0300

    Add transition on hover

--- frontend/src/components/ContainerClient/index.js
+++ frontend/src/components/ContainerClient/index.js
@@ -7,10 +7,14 @@ const ContainerClient = styled.div`
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
   padding: 30px;
   margin: 20px;
+  transition: all 0.2s ease-in-out;
 
   &:hover {
     color: #7159c1;
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
+    -webkit-transform: scale(1.05);
+    -ms-transform: scale(1.05);
+    transform: scale(1.05);
   }
 
   h1 {
